668c591d15c3a2b2e56547fc37007fd0d3e20b75
HvqEMAOU <hvqemaou@email.com>
2021-03-29 19:26:37 +0000

    Update Main.java

--- src/main/java/com/epam/test/automation/java/practice3/Main.java
+++ src/main/java/com/epam/test/automation/java/practice3/Main.java
@@ -2,14 +2,19 @@ package com.epam.test.automation.java.practice3;
 
 public class Main {
 
-    /**
-     * <summary>
-     * Implement code according to description of task 1.
-     * </summary>
-     */
+   
     public static int[] task1(int[] array) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        int[] result = new int[array.length];
+        for (int i = 0, z = array.length - 1; i <= z; i++, z--) {
+            if (array[i]%2==0 && array[z]%2==0){
+                result[z] = array[i];
+                result[i] = array[z];
+            }else{
+            result[i] = array[i];
+            result[z] = array[z];
+            }
+        }
+        return result;
     }
 
     /**
